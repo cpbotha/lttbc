78dba690a13b4be664eaeff8753590bd7b2d33af
Charl P. Botha <cpbotha@vxlabs.com>
2023-01-16 21:02:04 +0200

    Remove all non-essential parts of setup.py

--- setup.py
+++ setup.py
@@ -9,10 +9,6 @@ def get_script_path():
     return osp.dirname(osp.realpath(sys.argv[0]))
 
 
-def read(*parts):
-    return open(osp.join(get_script_path(), *parts)).read()
-
-
 class numpy_get_include:
     def __str__(self):
         import numpy
@@ -27,33 +23,4 @@ lttbc_py = Extension(
     include_dirs=[numpy_get_include(), get_script_path()],
 )
 
-setup(
-    name="lttbc",
-    author="European XFEL GmbH",
-    use_scm_version=True,
-    ext_modules=[lttbc_py],
-    author_email="dennis.goeries@xfel.eu",
-    maintainer="Dennis Goeries",
-    url="https://github.com/dgoeries/lttbc/",
-    description="Largest triangle three buckets module for Python written in C",
-    # this errors out during python -m build, so we remove it
-    #long_description=read("README.txt"),
-    long_description_content_type="text/markdown",
-    license="MIT",
-    #install_requires=["numpy~=1.21"],
-    #setup_requires=["numpy~=1.21", "setuptools_scm"],
-    python_requires=">=3.5",
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Console",
-        "Intended Audience :: Developers",
-        "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: Implementation :: CPython",
-        "Topic :: Scientific/Engineering :: Information Analysis",
-        "Topic :: Scientific/Engineering :: Physics",
-        "Topic :: Scientific/Engineering :: Visualization",
-    ],
-)
+setup(ext_modules=[lttbc_py])
